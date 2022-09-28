1ce6a0af2d262ab1a220c64b92b77d0850c7e595
Shaun Summerlin <mjolnrsoldier@gmail.com>
2022-09-28 00:44:15 -0400

    test

--- main/com/adventure/GameInputProcessor.java
+++ main/com/adventure/GameInputProcessor.java
@@ -3,247 +3,48 @@ package main.com.adventure;
 import main.com.adventure.settings.Command;
 import main.com.adventure.settings.CommandConstants;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Scanner;
-public class GameInputProcessor extends main.com.adventure.settings.CommandConstants implements GameInputProcessors {
-
-    public String processInput(Scanner scanner) {
-        String input;
-        Command command;
-        do {
-            input = prompt();
-            command = buildCommand(input);
-            if (command.getVerb().equals(CommandConstants.HELP)) {
-                System.out.println("Sorry, you can't ask for help at this time.");
-            }
-        } while (command.getVerb().equals(CommandConstants.HELP));
-        return input.slice([4, 9];
-       input .indexOf(input.charAt(input.length() - 1)));
-    }
-    public String prompt();
-        { System.out.print("What would you like to do? ");
-        return scanner.nextLine();
-        }
-    public Command buildCommand(String input) {
-        String[] words = input.split(" ");
-        if (words.length == 1) {
-            return buildSimpleCommand(words[0], "");
-        } else if (words.length == 2) {
-            return buildCommandWithObject(words[0], words[1]);
-        } else {
-            return buildSimpleCommand("invalid", "");
-        }
-
-    public Command buildSimpleCommand(String input, String objectName) {
-        return buildCommandWithObject(input, objectName);
-    }
-    public Command buildCommandWithObject(String input, String objectName)
-        if  buildCommandwithObject
-        {
-            return new Command(input, objectName);
-        } else {
-        return new Command(input,"");
-        null;
-        }
-    }
-}
-
-    static {
-        return prompt( "What would you like to do? " );
-    }
-        
-
-
-    /**
-     * This method will prompt the user for input.
-     * @return - the input from the user
-     */
-    private static main.com.adventure.settings.Command command
-            = new main.com.adventure.settings.Command();
-    public String prompt() ;
-    {
-        System.out.println("What would you like to do?");
-        return command.toString();
-    }
-}
-
-
-    /**
-     * This constructor takes in a Scanner object to be used for input.
-     */
-
-public class  setScanner( Scanner scanner ) {
-        ;p'[this.scanner = scanner];
-       Scanner  Scanner = new Scanner(System.in);
-         input.nextLine();
 
 
 
-        Locale locale = new Locale("en", "US");
-        scanner.useLocale(locale)
+public class GameInputProcessor {
+    private static final Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
 
-    {
-        return  GameInputProcessor();
-}
-
-    /*
-     * This method will return the next command entered by the user.
-     * @return - the next command entered by the user
-     */
-    public static Class getNextCommand(String command2);
-        { Class.nextLine
-        return command2;
-            return GameInputProcessor.command;
-
-
-        }
-
-
-
-    }
-
-    /**
-     * This method will get the user input from the Scanner object.
-     * @return - the user input from the Scanner object
-     */
-    public static main.com.adventure.settings.Command getUserInput() {
-        return command;
-    }
-
-    public static main.com.adventure.settings.Command prompt() {
-        Scanner prompt = new Scanner(System.in) ;
-        public void command;
-        command = new main.com.adventure.settings.Command(prompt);
-        final main.com.adventure.settings.Command prompt1 = prompt();
-        return prompt1;
-
-    }
-
-    public void voidString( main.com.adventure.settings.Command prompt("") ) {
-        System.out.println( prompt.toString() );
-        } {
-
-    return command.toString();
-}
-
-    /**
-     * This method will prompt the user for input.
-     * @return - the input from the user
-     */
-{
-    Scanner scanner() = new Scanner( System.in );
-        return GameInputProcessor;
-
-
-    }
-}
-    /*
-     * This constructor takes in a Scanner object to be used for input.
-     * @param scanner - the Scanner object to be used for input.
-     */
-    public GameInputProcessor(Scanner scanner){
-        this.scanner=scanner;
-       public Object object
-
-    }
-
-
-    /*
-      This function will take in the user input and return a Command object.
-      @return a Command object that contains the user input.
-     */
-    public Command getUserInput(input) {
-        String input = scanner.nextLine();
-        String[] inputArray = input.split(" ");
-
-        String verbString = inputArray[0].toUpperCase(Locale.ROOT);
-        CommandVerb verb = CommandVerb.getVerb(verbString);
-        String nounString = "";
-        if (inputArray.length > 1){
-        nounString=inputArray[1].toUpperCase(Locale.ROOT);
-
-
-        return new Class sentence(String verb, String nounString);
-        }
-
-
-public GameInputProcessor(String input) {
-        Scanner this.scanner = scanner;
-        return GameInputProcessor()
-    }
-
-    /**
-     * This function will take in the user input and return a Command object.
-     * @return a Command object that contains the user input.
-     */
-    public Command getUserInput() {
-        String input = scanner.nextLine();
-        String[] inputArray = input.split(" ");
-
-        String verbString = inputArray[0].toUpperCase(Locale.ROOT);
-        CommandVerb verb = CommandVerb.getVerb(verbString);
-        String nounString = "";
-        if (inputArray.length > 1) {
-            nounString = inputArray[1].toUpperCase(Locale.ROOT);
-        }
-
-        return new Command(verb, nounString);
-    }
-}
-
-package main.com.adventure;
-
-import main.com.adventure.settings.Command;
-import main.com.adventure.settings.CommandConstants;
-
-import java.util.Locale;
-import java.util.Scanner;
-public class GameInputProcessor extends main.com.adventure.settings.CommandConstants {
-{
-
-    /**
-     * This constructor takes in a Scanner object to be used for input.
-
-    **/
-    private  Command buildSimpleCommand(String input, String objectName) {
-        return
-
-        return ("look");
-
-    }
 
 
     /**
      * Asks the user for their next command.
-     *
      * @return the response from the user.
      */
-    @Override public String prompt() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Next Command: ");
-        String name = input.nextLine();
-        System.out.println("You entered: " + name);
-        input.close();
-        return name;
-    }
-
-    private
-    main.com.adventure.settings.Command getObjectName( String input ) {
-        Scanner scanner = new Scanner(input);
-    scanner.useLocale(Locale.ENGLISH);
-        String name = scanner.next();
-    }
-
-    {
-        final main.com.adventure.settings.Command command = new main.com.adventure.settings.Command( input );
-        return command;
+    public String prompt() {
+        System.out.println("Enter your next command:");
+        Scanner scanner = new Scanner(System.in);
+        String other = scanner.next();
+		return other;
 
     }
 
+    /**
+     * Inputs that come into this method represent single action with no object. When building the command, you'll want
+     * to supply the first word as the verb and leave the objectName blank.
+     * Example input:
+     *  "help"
+     *  "look"
+     *
+     *  Note: this command must stay private when running the tests
+     *
+     * @param input - the input from the user
+     * @return - the Command object with the proper verb and blank object
+     */
+    private Command buildSimpleCommand(String input) {
 
-    {
-        return buildCommandWithObject("");
 
+        String temp = input.substring(0, input.indexOf(' '));
+
+
+        return new Command(temp);
     }
     /**
      * Inputs that come into this method will have an object or objects that the action is acting on. You'll need to
@@ -264,32 +65,28 @@ public class GameInputProcessor extends main.com.adventure.settings.CommandConst
      * @param input - the input from the user
      * @return - the Command object with the proper verb and object
      */
-    private Command buildCommandWithObject(String input)
-    {  String[] arrCommand = input.split( " " );
-        arrC[input.split( " " ).length - 1];
-        String verb = arrCommand[0];
-        String object = arrCommand[1];
-        return new Command(verb, object);
-        if (arrC.length == 1) {
-            return new main.com.adventure.settings.CommandBuilder().setVerb( arrC[ 0 ] ).setObjectName( "" ).createCommand();
-        } else if (arrC.length >= 2) {
-            return new command(arrC[0], arrC[1]);
-        } else { final main.com.adventure.settings.Command command = new main.com.adventure.settings.Command( arrCommand[ 0 ] ))
-            new main.com.adventure.settings.CommandBuilder().setVerb( verb).setObjectName( "" ).createCommand();
-            final main.com.adventure.settings.Command command1 = command;
-            final main.com.adventure.settings.Command command2 = command1;
-            return command2;
+    private Command buildCommandWithObject(String input) {
+        if (input.split(" ").length > 1) {
+            String firstWord = input.split(" ") [0];
+            String secondWord = input.split(" ") [1];
+            return new Command(firstWord, secondWord);
+
+        } else {
+            String sand = input.trim();
+            return new Command(sand);
 
         }
     }
+
+
     /** DO NOT CHANGE ANYTHING BELOW THIS LINE. **/
 
     /**
      * Gets the next command from the user.
      * @return a command object
      */
-    public Command getNextCommand( int i ) {
-        String input = prompt(" Go to where Bernard BPO was located ");
+    public Command getNextCommand() {
+        String input = prompt();
         return processCommand(input);
     }
 
@@ -302,36 +99,8 @@ public class GameInputProcessor extends main.com.adventure.settings.CommandConst
         ) {
             return buildCommandWithObject(normalizedInput);
         } else {
-            return buildSimpleCommand.toString()(normalizedInput);
+            return buildSimpleCommand(normalizedInput);
         }
     }
 
-    public
-    void getCommand() {
-        return Command;
-    }
-
-    public
-    void setCommand( void command ) {
-        Command = command;
-    }
-
-    public class command extends main.com.adventure.settings.Command {
-    public main.com.adventure.settings.Command command(String verb)  {return main.com.adventure.GameInputProcessor.command)}null );}
-
-    public Command command( String verb, Class  objectName( ) {
-            super(verb, objectName);
-        }
-      command( org.checkerframework.checker.units.qual.s , String s1, int[] command ) {
-            int newSuper;
-          newSuper = ( x * 4 );
-          super( String newSuper, s1.toUpperCase() )
-
-
-
-        }
-
-
-    }
-    }
 }
