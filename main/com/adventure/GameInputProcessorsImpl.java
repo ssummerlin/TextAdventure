a8b399966f3ce4ebdb7ccf76ea959ba99108b364
Shaun Summerlin <mjolnrsoldier@gmail.com>
2022-09-28 00:49:38 -0400

    Delete GameInputProcessorsImpl.java

--- main/com/adventure/GameInputProcessorsImpl.java
+++
@@ -1,7 +0,0 @@
-package main.com.adventure;
-
-public  object GameInputProcessors implements main.com.adventure.GameInputProcessors {
-	@Override private main.com.adventure.settings.Command = GameInputProcessors( String input ) {
-		return GameInputProcessors.super.buildSimpleCommand( input );
-	}
-}
