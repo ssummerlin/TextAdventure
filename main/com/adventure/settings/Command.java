fa7ab807f6e4d981cd6fbe64d4e4914c1a1f23ff
Shaun Summerlin <mjolnrsoldier@gmail.com>
2022-09-28 01:35:14 -0400

    Update Command.java

--- main/com/adventure/settings/Command.java
+++ main/com/adventure/settings/Command.java
@@ -1,74 +1,51 @@
 package main.com.adventure.settings;
 
-public class Command < Context > {
+public class Command {
 
-    private   String verb;
-    protected String objectName;
-    private   String setVerb;
+    private String verb;
+    private String objectName;
 
     /**
      * Creates a command.
      * @param verb - the verb of the command
      * @param objectName - the object(s) to which the command is directed.
      */
-     **
+    public Command(String verb, String objectName) {
+        this.verb = verb;
+        this.objectName = objectName;
+    }
+
+    /**
      * Creates a command with no direct object (e.g. "help").
      * @param verb - the verb of the command
      */
-    public
-   Command( String verb ) {
+    public Command(String verb) {
         this.verb = verb;
         this.objectName = "";
     }
 
-    public
-    Command( android.content.Context context ) {
-        this( context, null );
-    }
-
-    public
-    Command( Context context, android.util.AttributeSet attrs ) {
-        this( context, attrs, 0 );
-    }
-
-    public
-    Command( Context context, AttributeSet attrs, int defStyle ) {
-        super( context, attrs, defStyle );
-        
-    }
-
-	public
-	Command(Context ) {
-
-	}
-
     public String getObjectName() {
         return objectName;
-
     }
-
-    
-
-    
-
-    @Override
-    public
-    String toString() {
-        return "Command" +                    "verb='"            + verb            + '\''            +            ", objectName='"            + objectName            + '\''            + " ";
-    }
-}
 
     public void setObjectName(String objectName) {
         this.objectName = objectName;
     }
-    public void setVerb(String verb) {
-        this.verb = verb; }
-    public String getVerb(String setVerb) ; {
-        this.setVerb = setVerb;
+
+    public String getVerb() {
         return verb;
-    }
-
 
     }
 
+    public void setVerb(String verb) {
+        this.verb = verb;
+    }
 
+    @Override
+    public String toString() {
+        return "Command{" +
+                "verb='" + verb + '\'' +
+                ", objectName='" + objectName + '\'' +
+                '}';
+    }
+}
